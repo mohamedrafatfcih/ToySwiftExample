5c36c1bdefafa65d6a592e92e6e3a82cdc5d238a
Mohamed Rafat <mohamedfafatfcih@gmail.com>
2023-12-04 22:07:15 +0200

    Class and Structs with inner types

--- ToySwiftExample/FirstGroup/newAnotherFile.swift
+++ ToySwiftExample/FirstGroup/newAnotherFile.swift
@@ -10,3 +10,39 @@ import Foundation
 func testAnotherFile(){
     
 }
+
+class A {
+    
+    class B {
+        func mB(){
+            print("mB")
+        }
+    }
+    
+    struct AS{
+        func mAS(){
+            print("mAS")
+        }
+    }
+    
+    func mA(){
+        print("mA")
+    }
+}
+
+
+struct S{
+    class SA{
+        
+    }
+    
+    struct SS{
+        func mSS(){
+            print("mSS")
+        }
+    }
+    
+    func mS(){
+        print("mS")
+    }
+}
