95196c251738f2aed85136168de63b1dcd48545e
Mohamed Rafat <mohamedfafatfcih@gmail.com>
2024-01-24 07:29:58 +0200

    Added two files

---
+++ ToySwiftExample/AddedFile.swift
@@ -0,0 +1,8 @@
+//
+//  AddedFile.swift
+//  ToySwiftExample
+//
+//  Created by MohamedRafat on 1/24/24.
+//
+
+import Foundation
---
+++ ToySwiftExample/FirstGroup/AddedFile2.swift
@@ -0,0 +1,8 @@
+//
+//  AddedFile2.swift
+//  ToySwiftExample
+//
+//  Created by MohamedRafat on 1/24/24.
+//
+
+import Foundation
