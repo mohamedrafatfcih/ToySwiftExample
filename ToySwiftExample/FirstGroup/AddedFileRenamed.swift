32e2e269d4a5b72b1cd1cd74a312a98a7b008287
Mohamed Rafat <mohamedrafatfcih@gmail.com>
2025-03-30 14:10:36 +0200

    Remove file and add another one

--- ToySwiftExample/FirstGroup/AddedFileRenamed.swift
+++
@@ -1,8 +0,0 @@
-//
-//  AddedFile2.swift
-//  ToySwiftExample
-//
-//  Created by MohamedRafat on 1/24/24.
-//
-
-import Foundation
---
+++ ToySwiftExample/ThirdGroup/newAddedFile.swift
@@ -0,0 +1,13 @@
+//
+//  newAddedFile.swift
+//  ToySwiftExample
+//
+//  Created by MohamedRafat on 3/30/25.
+//
+
+import Foundation
+
+struct NewFile {
+    let name: String
+    let lines: Int
+}
